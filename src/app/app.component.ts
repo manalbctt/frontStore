b3ad9ae2251cf3a5bf945e7bf9f88b40d8360e57
AYOUB BOUSSAIDI <Boussaidi-Ayoub@hotmail.com>
2024-05-29 18:36:37 +0100

    add routing to extract id store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router, RouterModule} from '@angular/router';
 
 
 @Component({
@@ -7,6 +7,37 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'firstAng';
+
+
+  id: any ;
+  storeDetails: any;
+
+  constructor(private route: ActivatedRoute, private router :Router) {
+  }
+
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.id = params.get('id');
+      let identif=Number(this.id);
+      if (this.id) {
+        localStorage.setItem('idStore',this.id);
+        console.log(this.id);
+        this.loadStoreDetails();
+        this.router.navigate(['/']);
+
+        //redirect to home Component
+      }
+    });
+  }
+
+  loadStoreDetails() {
+
+  }
+
+
+
+
 }

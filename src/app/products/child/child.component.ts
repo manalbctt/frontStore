20d05eb3cbf00276f4f504c287b06c489c53a921
Medovsky <ahmed123elbasri@gmail.com>
2024-05-31 14:59:00 +0100

    last commit

--- src/app/products/child/child.component.ts
+++ src/app/products/child/child.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ProductService } from 'src/app/product.service';
 
 @Component({
@@ -7,9 +8,21 @@ import { ProductService } from 'src/app/product.service';
   styleUrls: ['./child.component.scss']
 })
 export class ChildComponent {
+  /**
+   *
+   */
+  constructor(private router: Router) {
+    
+  }
   @Input() title: string="";
   @Input() image: string="";
   @Input() price: string="";
   @Input() id: number=0;
 
+   toHome(){
+    this.router.navigate(['/detailsproduct', this.id]).then(() => {
+      // Force a full page reload after navigation
+      window.location.href = "/detailsproduct/${this.id}";
+    });
+  }
 }
